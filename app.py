47a5551dbf9211ecc1a38dab0c1b3a63442a6b9c
nexo <65514701+whzinn@users.noreply.github.com>
2025-03-20 19:10:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def receive_webhook():
     return jsonify({"success": True})
 
 
-@app.route("/criar", methods=["POST"])
+@app.route("/criar", methods=["GET"])
 def criar():
   render_template("criar.html")
 
